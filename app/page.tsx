a32ac92dd84b03e90c8c86da0f5c69becabb8f1e
Calvin <calvingsx@gmail.com>
2024-07-06 13:50:15 -0300

    Ajustando estilo do botão de adicionar

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
-'use client'
+"use client";
 import { useThemeStore } from "@/store";
+import { Button } from "@/components/ui/button";
+import { Plus } from "@/icons";
 
 export default function Home() {
-  const toggleTheme = useThemeStore((state) => state.toggleTheme)
+  const toggleTheme = useThemeStore((state) => state.toggleTheme);
 
   return (
-    <main>      
-      <h1 className="text-2xl font-bold">Seus cartões</h1>      
+    <main>
+      <div className="flex flex-row gap-2 items-center">
+        <h1 className="text-2xl font-bold">Seus cartões</h1>
+        <Button variant={"outline"} size={"xsm"}>
+          <Plus className="i-sm mr-1" /> Novo cartão
+        </Button>
+      </div>
     </main>
   );
 }
