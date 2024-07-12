924301df2e589efb6775e5c0cfc4ac9fb1351699
Calvin <calvingsx@gmail.com>
2024-07-12 15:37:48 -0300

    Removendo feature de copy

--- components/Custom/containers/PaymentCardPreview.tsx
+++ components/Custom/containers/PaymentCardPreview.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import { Eye, EyeOff, Heart, Copy } from '@/icons';
+import { Eye, EyeOff, Heart } from '@/icons';
 
 interface PaymentCardPreview {
   title: string;
@@ -10,15 +10,15 @@ interface PaymentCardPreview {
 
 export default function PaymentCardPreview(props: PaymentCardPreview) {
   const [cardNumber, setCardNumber] = React.useState<PaymentCardPreview['cardNumber'] | null>(null);
-  const [maskedCardNumber, setMaskedCardNumber] = React.useState<string | null>(null)
+  const [maskedCardNumber, setMaskedCardNumber] = React.useState<string | null>(null);
   const [isMaskedCardNumber, setIsMaskedCardNumber] = React.useState<boolean>(true);
 
   React.useEffect(() => {
     const cardNumberWithDots = props.cardNumber.replace(/(\d{3})(?=\d)/g, '$1.');
-    setCardNumber(cardNumberWithDots)
+    setCardNumber(cardNumberWithDots);
     const cardNumberWithMaskAndDots = cardNumberWithDots.replace(/\d{3}(?=\.)/g, '***');
-    setMaskedCardNumber(cardNumberWithMaskAndDots)
-  }, [])
+    setMaskedCardNumber(cardNumberWithMaskAndDots);
+  }, []);
 
   return (
     <div className="border-primary sm:max-w-80 border-[0.1rem] px-4 py-2 rounded-sm transition-all ease-in-out duration-200 cursor-pointer box-shad gap-4 flex flex-col">
@@ -27,10 +27,7 @@ export default function PaymentCardPreview(props: PaymentCardPreview) {
         <Heart className="icon-md text-primary" />
       </div>
       <div className="flex flex-row justify-between items-center">
-        <div className="flex flex-row gap-2 items-center">
-          <p>{isMaskedCardNumber ? maskedCardNumber : cardNumber}</p>
-          <Copy className="icon-sm" />
-        </div>
+        <p>{isMaskedCardNumber ? maskedCardNumber : cardNumber}</p>
         {isMaskedCardNumber ? (
           <EyeOff onClick={() => setIsMaskedCardNumber(false)} className="icon-md" />
         ) : (
